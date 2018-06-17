fb65662650852f8261d19aa003f4e16302693a58
ExXeptional <elo95@gmx.de>
2018-06-17 22:22:49 +0200

    Removed Keil Proj Files

--- keil_proj_files/Objects/uart_core.d
+++
@@ -1,6 +0,0 @@
-.\objects\uart_core.o: ..\ports\tm4c123\uart_core.c
-.\objects\uart_core.o: D:\Keil_v5\ARM\ARMCC\Bin\..\include\string.h
-.\objects\uart_core.o: ..\..\Nmicropython\py/mpconfig.h
-.\objects\uart_core.o: ..\ports\tm4c123\mpconfigport.h
-.\objects\uart_core.o: D:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
-.\objects\uart_core.o: D:\Keil_v5\ARM\ARMCC\Bin\..\include\alloca.h
