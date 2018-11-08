bddcf3552d923062c23263429ca81ea9a69b0882
ExXeptional <elo95@gmx.de>
2018-11-08 15:20:08 +0100

    ports/tm4c123: New branch with stm32 base port

--- ports/tm4c123/mods/modnetwork.c
+++ ports/tm4c123/mods/modnetwork.c
@@ -3,8 +3,7 @@
  *
  * The MIT License (MIT)
  *
- * Copyright (c) 2013, 2014 Damien P. George
- * Copyright (c) 2015 Daniel Campora
+ * Copyright (c) 2014 Damien P. George
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -25,132 +24,93 @@
  * THE SOFTWARE.
  */
 
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+
+#include "py/objlist.h"
 #include "py/runtime.h"
-#include "py/mperrno.h"
 #include "py/mphal.h"
+#include "lib/netutils/netutils.h"
 #include "modnetwork.h"
-#include "mpexception.h"
-#include "serverstask.h"
-#include "simplelink.h"
 
+#if MICROPY_PY_NETWORK
 
-/******************************************************************************
- DEFINE TYPES
- ******************************************************************************/
-typedef struct {
-    mp_obj_base_t base;
-} network_server_obj_t;
+#if MICROPY_PY_LWIP
 
-/******************************************************************************
- DECLARE PRIVATE DATA
- ******************************************************************************/
-STATIC network_server_obj_t network_server_obj;
-STATIC const mp_obj_type_t network_server_type;
+#include "lwip/netif.h"
+#include "lwip/timeouts.h"
+#include "lwip/dns.h"
+#include "lwip/dhcp.h"
+
+u32_t sys_now(void) {
+    return mp_hal_ticks_ms();
+}
+
+void pyb_lwip_poll(void) {
+    // Poll all the NICs for incoming data
+    for (struct netif *netif = netif_list; netif != NULL; netif = netif->next) {
+        if (netif->flags & NETIF_FLAG_LINK_UP) {
+            mod_network_nic_type_t *nic = netif->state;
+            nic->poll_callback(nic, netif);
+        }
+    }
+    // Run the lwIP internal updates
+    sys_check_timeouts();
+}
+
+#endif
 
 /// \module network - network configuration
 ///
-/// This module provides network drivers and server configuration.
+/// This module provides network drivers and routing configuration.
 
-void mod_network_init0(void) {
+void mod_network_init(void) {
+    mp_obj_list_init(&MP_STATE_PORT(mod_network_nic_list), 0);
 }
 
-#if (MICROPY_PORT_HAS_TELNET || MICROPY_PORT_HAS_FTP)
-STATIC mp_obj_t network_server_init_helper(mp_obj_t self, const mp_arg_val_t *args) {
-    const char *user = SERVERS_DEF_USER;
-    const char *pass = SERVERS_DEF_PASS;
-    if (args[0].u_obj != MP_OBJ_NULL) {
-        mp_obj_t *login;
-        mp_obj_get_array_fixed_n(args[0].u_obj, 2, &login);
-        user = mp_obj_str_get_str(login[0]);
-        pass = mp_obj_str_get_str(login[1]);
-    }
-
-    uint32_t timeout = SERVERS_DEF_TIMEOUT_MS / 1000;
-    if (args[1].u_obj != MP_OBJ_NULL) {
-        timeout = mp_obj_get_int(args[1].u_obj);
-    }
-
-    // configure the new login
-    servers_set_login ((char *)user, (char *)pass);
-
-    // configure the timeout
-    servers_set_timeout(timeout * 1000);
-
-    // start the servers
-    servers_start();
-
-    return mp_const_none;
+void mod_network_deinit(void) {
 }
 
-STATIC const mp_arg_t network_server_args[] = {
-    { MP_QSTR_id,                            MP_ARG_OBJ, {.u_obj = MP_OBJ_NULL} },
-    { MP_QSTR_login,        MP_ARG_KW_ONLY | MP_ARG_OBJ, {.u_obj = MP_OBJ_NULL} },
-    { MP_QSTR_timeout,      MP_ARG_KW_ONLY | MP_ARG_OBJ, {.u_obj = MP_OBJ_NULL} },
-};
-STATIC mp_obj_t network_server_make_new(const mp_obj_type_t *type, size_t n_args, size_t n_kw, const mp_obj_t *all_args) {
-    // parse args
-    mp_map_t kw_args;
-    mp_map_init_fixed_table(&kw_args, n_kw, all_args + n_args);
-    mp_arg_val_t args[MP_ARRAY_SIZE(network_server_args)];
-    mp_arg_parse_all(n_args, all_args, &kw_args, MP_ARRAY_SIZE(args), network_server_args, args);
-
-    // check the server id
-    if (args[0].u_obj != MP_OBJ_NULL) {
-        if (mp_obj_get_int(args[0].u_obj) != 0) {
-            mp_raise_OSError(MP_ENODEV);
+void mod_network_register_nic(mp_obj_t nic) {
+    for (mp_uint_t i = 0; i < MP_STATE_PORT(mod_network_nic_list).len; i++) {
+        if (MP_STATE_PORT(mod_network_nic_list).items[i] == nic) {
+            // nic already registered
+            return;
         }
     }
-
-    // setup the object and initialize it
-    network_server_obj_t *self = &network_server_obj;
-    self->base.type = &network_server_type;
-    network_server_init_helper(self, &args[1]);
-
-    return (mp_obj_t)self;
+    // nic not registered so add to list
+    mp_obj_list_append(MP_OBJ_FROM_PTR(&MP_STATE_PORT(mod_network_nic_list)), nic);
 }
 
-STATIC mp_obj_t network_server_init(size_t n_args, const mp_obj_t *pos_args, mp_map_t *kw_args) {
-    // parse args
-    mp_arg_val_t args[MP_ARRAY_SIZE(network_server_args) - 1];
-    mp_arg_parse_all(n_args - 1, pos_args + 1, kw_args, MP_ARRAY_SIZE(args), &network_server_args[1], args);
-    return network_server_init_helper(pos_args[0], args);
-}
-STATIC MP_DEFINE_CONST_FUN_OBJ_KW(network_server_init_obj, 1, network_server_init);
-
-// timeout value given in seconds
-STATIC mp_obj_t network_server_timeout(size_t n_args, const mp_obj_t *args) {
-    if (n_args > 1) {
-        uint32_t timeout = mp_obj_get_int(args[1]);
-        servers_set_timeout(timeout * 1000);
-        return mp_const_none;
-    } else {
-        // get
-        return mp_obj_new_int(servers_get_timeout() / 1000);
+mp_obj_t mod_network_find_nic(const uint8_t *ip) {
+    // find a NIC that is suited to given IP address
+    for (mp_uint_t i = 0; i < MP_STATE_PORT(mod_network_nic_list).len; i++) {
+        mp_obj_t nic = MP_STATE_PORT(mod_network_nic_list).items[i];
+        // TODO check IP suitability here
+        //mod_network_nic_type_t *nic_type = (mod_network_nic_type_t*)mp_obj_get_type(nic);
+        return nic;
     }
-}
-STATIC MP_DEFINE_CONST_FUN_OBJ_VAR_BETWEEN(network_server_timeout_obj, 1, 2, network_server_timeout);
 
-STATIC mp_obj_t network_server_running(mp_obj_t self_in) {
-    // get
-    return mp_obj_new_bool(servers_are_enabled());
+    nlr_raise(mp_obj_new_exception_msg(&mp_type_OSError, "no available NIC"));
 }
-STATIC MP_DEFINE_CONST_FUN_OBJ_1(network_server_running_obj, network_server_running);
 
-STATIC mp_obj_t network_server_deinit(mp_obj_t self_in) {
-    // simply stop the servers
-    servers_stop();
-    return mp_const_none;
+STATIC mp_obj_t network_route(void) {
+    return MP_OBJ_FROM_PTR(&MP_STATE_PORT(mod_network_nic_list));
 }
-STATIC MP_DEFINE_CONST_FUN_OBJ_1(network_server_deinit_obj, network_server_deinit);
-#endif
+STATIC MP_DEFINE_CONST_FUN_OBJ_0(network_route_obj, network_route);
 
 STATIC const mp_rom_map_elem_t mp_module_network_globals_table[] = {
-    { MP_ROM_QSTR(MP_QSTR___name__),            MP_ROM_QSTR(MP_QSTR_network) },
-    { MP_ROM_QSTR(MP_QSTR_WLAN),                MP_ROM_PTR(&mod_network_nic_type_wlan) },
+    { MP_ROM_QSTR(MP_QSTR___name__), MP_ROM_QSTR(MP_QSTR_network) },
 
-#if (MICROPY_PORT_HAS_TELNET || MICROPY_PORT_HAS_FTP)
-    { MP_ROM_QSTR(MP_QSTR_Server),              MP_ROM_PTR(&network_server_type) },
-#endif
+    #if MICROPY_PY_WIZNET5K
+    { MP_ROM_QSTR(MP_QSTR_WIZNET5K), MP_ROM_PTR(&mod_network_nic_type_wiznet5k) },
+    #endif
+    #if MICROPY_PY_CC3K
+    { MP_ROM_QSTR(MP_QSTR_CC3K), MP_ROM_PTR(&mod_network_nic_type_cc3k) },
+    #endif
+
+    { MP_ROM_QSTR(MP_QSTR_route), MP_ROM_PTR(&network_route_obj) },
 };
 
 STATIC MP_DEFINE_CONST_DICT(mp_module_network_globals, mp_module_network_globals_table);
@@ -160,20 +120,58 @@ const mp_obj_module_t mp_module_network = {
     .globals = (mp_obj_dict_t*)&mp_module_network_globals,
 };
 
-#if (MICROPY_PORT_HAS_TELNET || MICROPY_PORT_HAS_FTP)
-STATIC const mp_rom_map_elem_t network_server_locals_dict_table[] = {
-    { MP_ROM_QSTR(MP_QSTR_init),                MP_ROM_PTR(&network_server_init_obj) },
-    { MP_ROM_QSTR(MP_QSTR_deinit),              MP_ROM_PTR(&network_server_deinit_obj) },
-    { MP_ROM_QSTR(MP_QSTR_timeout),             MP_ROM_PTR(&network_server_timeout_obj) },
-    { MP_ROM_QSTR(MP_QSTR_isrunning),           MP_ROM_PTR(&network_server_running_obj) },
-};
+/*******************************************************************************/
+// Implementations of network methods that can be used by any interface
 
-STATIC MP_DEFINE_CONST_DICT(network_server_locals_dict, network_server_locals_dict_table);
+#if MICROPY_PY_LWIP
 
-STATIC const mp_obj_type_t network_server_type = {
-    { &mp_type_type },
-    .name = MP_QSTR_Server,
-    .make_new = network_server_make_new,
-    .locals_dict = (mp_obj_t)&network_server_locals_dict,
-};
+mp_obj_t mod_network_nic_ifconfig(struct netif *netif, size_t n_args, const mp_obj_t *args) {
+    if (n_args == 0) {
+        // Get IP addresses
+        const ip_addr_t *dns = dns_getserver(0);
+        mp_obj_t tuple[4] = {
+            netutils_format_ipv4_addr((uint8_t*)&netif->ip_addr, NETUTILS_BIG),
+            netutils_format_ipv4_addr((uint8_t*)&netif->netmask, NETUTILS_BIG),
+            netutils_format_ipv4_addr((uint8_t*)&netif->gw, NETUTILS_BIG),
+            netutils_format_ipv4_addr((uint8_t*)dns, NETUTILS_BIG),
+        };
+        return mp_obj_new_tuple(4, tuple);
+    } else if (args[0] == MP_OBJ_NEW_QSTR(MP_QSTR_dhcp)) {
+        // Start the DHCP client
+        if (dhcp_supplied_address(netif)) {
+            dhcp_renew(netif);
+        } else {
+            dhcp_stop(netif);
+            dhcp_start(netif);
+        }
+
+        // Wait for DHCP to get IP address
+        uint32_t start = mp_hal_ticks_ms();
+        while (!dhcp_supplied_address(netif)) {
+            if (mp_hal_ticks_ms() - start > 10000) {
+                mp_raise_msg(&mp_type_OSError, "timeout waiting for DHCP to get IP address");
+            }
+            mp_hal_delay_ms(100);
+        }
+
+        return mp_const_none;
+    } else {
+        // Release and stop any existing DHCP
+        dhcp_release(netif);
+        dhcp_stop(netif);
+        // Set static IP addresses
+        mp_obj_t *items;
+        mp_obj_get_array_fixed_n(args[0], 4, &items);
+        netutils_parse_ipv4_addr(items[0], (uint8_t*)&netif->ip_addr, NETUTILS_BIG);
+        netutils_parse_ipv4_addr(items[1], (uint8_t*)&netif->netmask, NETUTILS_BIG);
+        netutils_parse_ipv4_addr(items[2], (uint8_t*)&netif->gw, NETUTILS_BIG);
+        ip_addr_t dns;
+        netutils_parse_ipv4_addr(items[3], (uint8_t*)&dns, NETUTILS_BIG);
+        dns_setserver(0, &dns);
+        return mp_const_none;
+    }
+}
+
 #endif
+
+#endif  // MICROPY_PY_NETWORK
--- ports/tm4c123/mods/modutime.c
+++ ports/tm4c123/mods/modutime.c
@@ -4,7 +4,6 @@
  * The MIT License (MIT)
  *
  * Copyright (c) 2013, 2014 Damien P. George
- * Copyright (c) 2015 Daniel Campora
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -28,36 +27,25 @@
 #include <stdio.h>
 #include <string.h>
 
-#include "py/mpconfig.h"
 #include "py/runtime.h"
-#include "py/obj.h"
 #include "py/smallint.h"
-#include "py/mphal.h"
+#include "py/obj.h"
 #include "lib/timeutils/timeutils.h"
 #include "extmod/utime_mphal.h"
-#include "inc/hw_types.h"
-#include "inc/hw_ints.h"
-#include "inc/hw_memmap.h"
-#include "rom_map.h"
-#include "prcm.h"
 #include "systick.h"
-#include "pybrtc.h"
-#include "mpexception.h"
-#include "utils.h"
+#include "portmodules.h"
+#include "rtc.h"
 
 /// \module time - time related functions
 ///
 /// The `time` module provides functions for getting the current time and date,
 /// and for sleeping.
 
-/******************************************************************************/
-// MicroPython bindings
-
 /// \function localtime([secs])
 /// Convert a time expressed in seconds since Jan 1, 2000 into an 8-tuple which
 /// contains: (year, month, mday, hour, minute, second, weekday, yearday)
 /// If secs is not provided or None, then the current time from the RTC is used.
-/// year includes the century (for example 2015)
+/// year includes the century (for example 2014)
 /// month   is 1-12
 /// mday    is 1-31
 /// hour    is 0-23
@@ -67,19 +55,22 @@
 /// yearday is 1-366
 STATIC mp_obj_t time_localtime(size_t n_args, const mp_obj_t *args) {
     if (n_args == 0 || args[0] == mp_const_none) {
-        timeutils_struct_time_t tm;
-
-        // get the seconds from the RTC
-        timeutils_seconds_since_2000_to_struct_time(pyb_rtc_get_seconds(), &tm);
+        // get current date and time
+        // note: need to call get time then get date to correctly access the registers
+        rtc_init_finalise();
+        RTC_DateTypeDef date;
+        RTC_TimeTypeDef time;
+        HAL_RTC_GetTime(&RTCHandle, &time, RTC_FORMAT_BIN);
+        HAL_RTC_GetDate(&RTCHandle, &date, RTC_FORMAT_BIN);
         mp_obj_t tuple[8] = {
-                mp_obj_new_int(tm.tm_year),
-                mp_obj_new_int(tm.tm_mon),
-                mp_obj_new_int(tm.tm_mday),
-                mp_obj_new_int(tm.tm_hour),
-                mp_obj_new_int(tm.tm_min),
-                mp_obj_new_int(tm.tm_sec),
-                mp_obj_new_int(tm.tm_wday),
-                mp_obj_new_int(tm.tm_yday)
+            mp_obj_new_int(2000 + date.Year),
+            mp_obj_new_int(date.Month),
+            mp_obj_new_int(date.Date),
+            mp_obj_new_int(time.Hours),
+            mp_obj_new_int(time.Minutes),
+            mp_obj_new_int(time.Seconds),
+            mp_obj_new_int(date.WeekDay - 1),
+            mp_obj_new_int(timeutils_year_day(2000 + date.Year, date.Month, date.Date)),
         };
         return mp_obj_new_tuple(8, tuple);
     } else {
@@ -101,7 +92,13 @@ STATIC mp_obj_t time_localtime(size_t n_args, const mp_obj_t *args) {
 }
 MP_DEFINE_CONST_FUN_OBJ_VAR_BETWEEN(time_localtime_obj, 0, 1, time_localtime);
 
+
+/// \function mktime()
+/// This is inverse function of localtime. It's argument is a full 8-tuple
+/// which expresses a time as per localtime. It returns an integer which is
+/// the number of seconds since Jan 1, 2000.
 STATIC mp_obj_t time_mktime(mp_obj_t tuple) {
+
     size_t len;
     mp_obj_t *elem;
 
@@ -109,44 +106,43 @@ STATIC mp_obj_t time_mktime(mp_obj_t tuple) {
 
     // localtime generates a tuple of len 8. CPython uses 9, so we accept both.
     if (len < 8 || len > 9) {
-        mp_raise_TypeError(mpexception_num_type_invalid_arguments);
+        nlr_raise(mp_obj_new_exception_msg_varg(&mp_type_TypeError, "mktime needs a tuple of length 8 or 9 (%d given)", len));
     }
 
-    return mp_obj_new_int_from_uint(timeutils_mktime(mp_obj_get_int(elem[0]), mp_obj_get_int(elem[1]), mp_obj_get_int(elem[2]),
-                                                     mp_obj_get_int(elem[3]), mp_obj_get_int(elem[4]), mp_obj_get_int(elem[5])));
+    return mp_obj_new_int_from_uint(timeutils_mktime(mp_obj_get_int(elem[0]),
+            mp_obj_get_int(elem[1]), mp_obj_get_int(elem[2]), mp_obj_get_int(elem[3]),
+            mp_obj_get_int(elem[4]), mp_obj_get_int(elem[5])));
 }
 MP_DEFINE_CONST_FUN_OBJ_1(time_mktime_obj, time_mktime);
 
+/// \function time()
+/// Returns the number of seconds, as an integer, since 1/1/2000.
 STATIC mp_obj_t time_time(void) {
-    return mp_obj_new_int(pyb_rtc_get_seconds());
+    // get date and time
+    // note: need to call get time then get date to correctly access the registers
+    rtc_init_finalise();
+    RTC_DateTypeDef date;
+    RTC_TimeTypeDef time;
+    HAL_RTC_GetTime(&RTCHandle, &time, RTC_FORMAT_BIN);
+    HAL_RTC_GetDate(&RTCHandle, &date, RTC_FORMAT_BIN);
+    return mp_obj_new_int(timeutils_seconds_since_2000(2000 + date.Year, date.Month, date.Date, time.Hours, time.Minutes, time.Seconds));
 }
 MP_DEFINE_CONST_FUN_OBJ_0(time_time_obj, time_time);
 
-STATIC mp_obj_t time_sleep(mp_obj_t seconds_o) {
-    int32_t sleep_s = mp_obj_get_int(seconds_o);
-    if (sleep_s > 0) {
-        mp_hal_delay_ms(sleep_s * 1000);
-    }
-    return mp_const_none;
-}
-MP_DEFINE_CONST_FUN_OBJ_1(time_sleep_obj, time_sleep);
-
 STATIC const mp_rom_map_elem_t time_module_globals_table[] = {
-    { MP_ROM_QSTR(MP_QSTR___name__),        MP_ROM_QSTR(MP_QSTR_utime) },
+    { MP_ROM_QSTR(MP_QSTR___name__), MP_ROM_QSTR(MP_QSTR_utime) },
 
-    { MP_ROM_QSTR(MP_QSTR_localtime),       MP_ROM_PTR(&time_localtime_obj) },
-    { MP_ROM_QSTR(MP_QSTR_mktime),          MP_ROM_PTR(&time_mktime_obj) },
-    { MP_ROM_QSTR(MP_QSTR_time),            MP_ROM_PTR(&time_time_obj) },
-    { MP_ROM_QSTR(MP_QSTR_sleep),           MP_ROM_PTR(&time_sleep_obj) },
-
-    // MicroPython additions
-    { MP_ROM_QSTR(MP_QSTR_sleep_ms),        MP_ROM_PTR(&mp_utime_sleep_ms_obj) },
-    { MP_ROM_QSTR(MP_QSTR_sleep_us),        MP_ROM_PTR(&mp_utime_sleep_us_obj) },
-    { MP_ROM_QSTR(MP_QSTR_ticks_ms),        MP_ROM_PTR(&mp_utime_ticks_ms_obj) },
-    { MP_ROM_QSTR(MP_QSTR_ticks_us),        MP_ROM_PTR(&mp_utime_ticks_us_obj) },
-    { MP_ROM_QSTR(MP_QSTR_ticks_cpu),       MP_ROM_PTR(&mp_utime_ticks_cpu_obj) },
-    { MP_ROM_QSTR(MP_QSTR_ticks_add),       MP_ROM_PTR(&mp_utime_ticks_add_obj) },
-    { MP_ROM_QSTR(MP_QSTR_ticks_diff),      MP_ROM_PTR(&mp_utime_ticks_diff_obj) },
+    { MP_ROM_QSTR(MP_QSTR_localtime), MP_ROM_PTR(&time_localtime_obj) },
+    { MP_ROM_QSTR(MP_QSTR_mktime), MP_ROM_PTR(&time_mktime_obj) },
+    { MP_ROM_QSTR(MP_QSTR_time), MP_ROM_PTR(&time_time_obj) },
+    { MP_ROM_QSTR(MP_QSTR_sleep), MP_ROM_PTR(&mp_utime_sleep_obj) },
+    { MP_ROM_QSTR(MP_QSTR_sleep_ms), MP_ROM_PTR(&mp_utime_sleep_ms_obj) },
+    { MP_ROM_QSTR(MP_QSTR_sleep_us), MP_ROM_PTR(&mp_utime_sleep_us_obj) },
+    { MP_ROM_QSTR(MP_QSTR_ticks_ms), MP_ROM_PTR(&mp_utime_ticks_ms_obj) },
+    { MP_ROM_QSTR(MP_QSTR_ticks_us), MP_ROM_PTR(&mp_utime_ticks_us_obj) },
+    { MP_ROM_QSTR(MP_QSTR_ticks_cpu), MP_ROM_PTR(&mp_utime_ticks_cpu_obj) },
+    { MP_ROM_QSTR(MP_QSTR_ticks_add), MP_ROM_PTR(&mp_utime_ticks_add_obj) },
+    { MP_ROM_QSTR(MP_QSTR_ticks_diff), MP_ROM_PTR(&mp_utime_ticks_diff_obj) },
 };
 
 STATIC MP_DEFINE_CONST_DICT(time_module_globals, time_module_globals_table);
