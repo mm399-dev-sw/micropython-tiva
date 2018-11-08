c6a787f397851913c4ea48cf748f454bcf41e43c
Raphael Kriegl <elo95@gmx.de>
2018-11-08 16:35:31 +0100

    ports/tm4c123: Removed TivaWare folder

--- ports/tm4c123/mods/pin_defs_stm32.h
+++ ports/tm4c123/mods/pin_defs_stm32.h
@@ -24,8 +24,126 @@
  * THE SOFTWARE.
  */
 
-// This file contains pin definitions that are specific to the stm32 port.
+// This file contains pin definitions that are specific to the tm4c123 port.
 // This file should only ever be #included by pin.h and not directly.
+
+#include "inc/hw_gpio.h"
+#include "driverlib/gpio.h"
+
+enum {
+    PORT_A = GPIO_PORTA_AHB_BASE,
+    PORT_B = GPIO_PORTB_AHB_BASE,
+    PORT_C = GPIO_PORTC_AHB_BASE,
+    PORT_D = GPIO_PORTD_AHB_BASE,
+    PORT_E = GPIO_PORTE_AHB_BASE,
+    PORT_F = GPIO_PORTF_AHB_BASE,
+};
+
+enum {
+    PIN_FN_ADC = 0,
+    PIN_FN_UART,
+    PIN_FN_SSI,
+    PIN_FN_I2C,
+    PIN_FN_QEI,
+    PIN_FN_MTRL,
+    PIN_FN_WTIM,
+    PIN_FN_TIM,
+    PIN_FN_CAN,
+    PIN_FN_USB,
+    PIN_FN_COMP,
+    PIN_FN_TR = 14,
+};
+
+enum {
+    AF_PIN_TYPE_UART_TX = 0,
+    AF_PIN_TYPE_UART_RX,
+    AF_PIN_TYPE_UART_RTS,
+    AF_PIN_TYPE_UART_CTS,
+};
+
+enum {
+    AF_PIN_TYPE_SSI_CLK = 0,
+    AF_PIN_TYPE_SSI_TX,
+    AF_PIN_TYPE_SSI_RX,
+    AF_PIN_TYPE_SSI_FSS,
+};
+
+enum {
+    AF_PIN_TYPE_I2C_SDA = 0,
+    AF_PIN_TYPE_I2C_SCL,
+};
+
+enum {
+    AF_PIN_TYPE_TIM_CCP0 = 0,
+    AF_PIN_TYPE_TIM_CCP1
+};
+
+enum {
+    AF_PIN_TYPE_WTIM_CCP0 = 0,
+    AF_PIN_TYPE_WTIM_CCP1
+};
+
+enum {
+    AF_PIN_TYPE_QEI_PHA0 = 0,
+    AF_PIN_TYPE_QEI_PHA1,
+    AF_PIN_TYPE_QEI_PHB0,
+    AF_PIN_TYPE_QEI_PHB1,
+    AF_PIN_TYPE_QEI_IDX0,
+    AF_PIN_TYPE_QEI_IDX1
+};
+
+enum {
+    AF_PIN_TYPE_USB_EPEN = 0,
+    AF_PIN_TYPE_USB_PFLT,
+    AF_PIN_TYPE_USB_DM,
+    AF_PIN_TYPE_USB_DP,
+    AF_PIN_TYPE_USB_ID,
+    AF_PIN_TYPE_USB_VBUS
+};
+
+enum {
+    AF_PIN_TYPE_COMP_POS = 0,
+    AF_PIN_TYPE_COMP_NEG,
+    AF_PIN_TYPE_COMP_OUT
+};
+
+enum {
+    AF_PIN_TYPE_CAN_TX = 0,
+    AF_PIN_TYPE_CAN_RX
+};
+
+enum {
+    AF_PIN_TYPE_TR_CLK = 0,
+    AF_PIN_TYPE_TR_D0,
+    AF_PIN_TYPE_TR_D1
+};
+
+enum {
+    AF_PIN_TYPE_MTRL_FAULT0 = 0,
+    AF_PIN_TYPE_MTRL_PWM0 ,
+    AF_PIN_TYPE_MTRL_PWM1 ,
+    AF_PIN_TYPE_MTRL_PWM2 ,
+    AF_PIN_TYPE_MTRL_PWM3 ,
+    AF_PIN_TYPE_MTRL_PWM4 ,
+    AF_PIN_TYPE_MTRL_PWM5 ,
+    AF_PIN_TYPE_MTRL_PWM6 ,
+    AF_PIN_TYPE_MTRL_PWM7 ,
+};
+
+enum {
+    AF_PIN_TYPE_ADC_AIN0 = 0,
+    AF_PIN_TYPE_ADC_AIN1,
+    AF_PIN_TYPE_ADC_AIN2,
+    AF_PIN_TYPE_ADC_AIN3,
+    AF_PIN_TYPE_ADC_AIN4,
+    AF_PIN_TYPE_ADC_AIN5,
+    AF_PIN_TYPE_ADC_AIN6,
+    AF_PIN_TYPE_ADC_AIN7,
+    AF_PIN_TYPE_ADC_AIN8,
+    AF_PIN_TYPE_ADC_AIN9,
+    AF_PIN_TYPE_ADC_AIN10,
+    AF_PIN_TYPE_ADC_AIN11,
+};
 
 enum {
   PORT_A,
