134cf2f39102f9557a62a2f1d5e7a6f0d9c3a72a
Raphael Kriegl <elo95@gmx.de>
2019-02-17 19:15:11 +0100

    ports/tm4c123: finished modutime with some adaptions

--- ports/tm4c123/mods/modutime.c
+++ ports/tm4c123/mods/modutime.c
@@ -35,12 +35,18 @@
 #include "systick.h"
 #include "portmodules.h"
 #include "rtc.h"
+#include "time.h"
 #include "driverlib/hibernate.h"
 
 /// \module time - time related functions
 ///
 /// The `time` module provides functions for getting the current time and date,
 /// and for sleeping.
+
+
+// For converting rtc time to hours and minutes
+#define SEC_HOUR 3600
+#define SEC_MINUTE 60
 
 /// \function localtime([secs])
 /// Convert a time expressed in seconds since Jan 1, 2000 into an 8-tuple which
@@ -59,19 +65,16 @@ STATIC mp_obj_t time_localtime(size_t n_args, const mp_obj_t *args) {
         // get current date and time
         // note: need to call get time then get date to correctly access the registers
         rtc_init_finalise();
-        RTC_DateTypeDef date;
-        RTC_TimeTypeDef time;
-        HAL_RTC_GetTime(&RTCHandle, &time, RTC_FORMAT_BIN);
-        HAL_RTC_GetDate(&RTCHandle, &date, RTC_FORMAT_BIN);
+        uint32_t time = HibernateRTCGet();
         mp_obj_t tuple[8] = {
-            mp_obj_new_int(2000 + date.Year),
-            mp_obj_new_int(date.Month),
-            mp_obj_new_int(date.Date),
-            mp_obj_new_int(time.Hours),
-            mp_obj_new_int(time.Minutes),
-            mp_obj_new_int(time.Seconds),
-            mp_obj_new_int(date.WeekDay - 1),
-            mp_obj_new_int(timeutils_year_day(2000 + date.Year, date.Month, date.Date)),
+             mp_obj_new_int(2018),
+             mp_obj_new_int(1),
+             mp_obj_new_int(1),
+             mp_obj_new_int((time / SEC_HOUR) % 24),
+             mp_obj_new_int((time / SEC_MINUTE) % 60),
+             mp_obj_new_int(time % 60),
+             mp_obj_new_int(1 - 1),
+             mp_obj_new_int(timeutils_year_day(2018, 1, 1)),
         };
         return mp_obj_new_tuple(8, tuple);
     } else {
@@ -122,11 +125,8 @@ STATIC mp_obj_t time_time(void) {
     // get date and time
     // note: need to call get time then get date to correctly access the registers
     rtc_init_finalise();
-    RTC_DateTypeDef date;
-    RTC_TimeTypeDef time;
-    HAL_RTC_GetTime(&RTCHandle, &time, RTC_FORMAT_BIN);
-    HAL_RTC_GetDate(&RTCHandle, &date, RTC_FORMAT_BIN);
-    return mp_obj_new_int(timeutils_seconds_since_2000(2000 + date.Year, date.Month, date.Date, time.Hours, time.Minutes, time.Seconds));
+    uint32_t time = HibernateRTCGet();
+    return mp_obj_new_int(timeutils_seconds_since_2000(2018, 1, 1, (time / SEC_HOUR) % 24, (time / SEC_MINUTE) % 60, time % 60));
 }
 MP_DEFINE_CONST_FUN_OBJ_0(time_time_obj, time_time);
 
