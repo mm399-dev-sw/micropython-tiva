3b0116263e7c26b7fa8f2238fe41eaf4a10f96e1
Raphael Kriegl <elo95@gmx.de>
2019-02-18 20:55:24 +0100

    ports/tm4c123: removed minimal startup from main

--- ports/tm4c123/main.c
+++ ports/tm4c123/main.c
@@ -174,52 +174,6 @@ void MP_WEAK __assert_func(const char *file, int line, const char *func, const c
 #endif
 
 #if MICROPY_MIN_USE_CORTEX_CPU
-
-// this is a minimal IRQ and reset framework for any Cortex-M CPU
-//
-//extern uint32_t _estack, _sidata, _sdata, _edata, _sbss, _ebss;
-//
-//void Reset_Handler(void) __attribute__((naked));
-//void Reset_Handler(void) {
-//    // set stack pointer
-//    __asm volatile ("ldr sp, =_estack");
-//    // copy .data section from flash to RAM
-//    for (uint32_t *src = &_sidata, *dest = &_sdata; dest < &_edata;) {
-//        *dest++ = *src++;
-//    }
-//    // zero out .bss section
-//    for (uint32_t *dest = &_sbss; dest < &_ebss;) {
-//        *dest++ = 0;
-//    }
-//    // jump to board initialisation
-//    void _start(void);
-//    _start();
-//}
-//
-//void Default_Handler(void) {
-//    for (;;) {
-//    }
-//}
-//
-//const uint32_t isr_vector[] __attribute__((section(".isr_vector"))) = {
-//    (uint32_t)&_estack,
-//    (uint32_t)&Reset_Handler,
-//    (uint32_t)&Default_Handler, // NMI_Handler
-//    (uint32_t)&Default_Handler, // HardFault_Handler
-//    (uint32_t)&Default_Handler, // MemManage_Handler
-//    (uint32_t)&Default_Handler, // BusFault_Handler
-//    (uint32_t)&Default_Handler, // UsageFault_Handler
-//    0,
-//    0,
-//    0,
-//    0,
-//    (uint32_t)&Default_Handler, // SVC_Handler
-//    (uint32_t)&Default_Handler, // DebugMon_Handler
-//    0,
-//    (uint32_t)&Default_Handler, // PendSV_Handler
-//    (uint32_t)&Default_Handler, // SysTick_Handler
-//};
-
 void _start(void) {
     // when we get here: stack is initialised, bss is clear, data is copied
 
