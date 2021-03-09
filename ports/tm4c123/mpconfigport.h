bda63519217f3eadb718d140c7e7746a15c13d8e
Raphael <40037381+rk-exxec@users.noreply.github.com>
2021-03-09 20:08:46 +0100

    ports/tm4c123: include i2c in machine module

--- ports/tm4c123/mpconfigport.h
+++ ports/tm4c123/mpconfigport.h
@@ -89,6 +89,8 @@
 
 // sd card
 #define MICROPY_HW_HAS_SDCARD       (1)
+#define MICROPY_PY_MACHINE_HW_SPI      (1)
+#define MICROPY_PY_MACHINE_HW_I2C      (1)
 
 // fatfs configuration used in ffconf.h
 #define MICROPY_FATFS_ENABLE_LFN       (1)
